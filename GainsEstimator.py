bc6740bce2c8b3c7fb943a0fa15dfaec3ba2441b
rrtucci <tucci@ar-tiste.com>
2023-02-09 00:01:14 -0500

    first commit

--- GainsEstimator.py
+++ GainsEstimator.py
@@ -14,27 +14,39 @@ class GainsEstimator:
     input file that contains a dataset. The dataset has the node names
     graph.ord_nodes as column labels, and instances of node values in each row.
 
+    A list of hidden nodes is an argument of the class constructor with None
+    as default value. Columns of the input dataset corresponding to hidden
+    nodes will be ignored. Hence, these column entries can be any number.
+    Correlations <x_i, x_j> where x_i or x_j is a hidden node will be
+    expressed symbolically; otherwise, they will be expressed numerically.
+
     Attributes
     ----------
     alp_mat_estimate: np.array of shape=(dim, dim), where dim=number of nodes
         estimate of the alpha matrix. Contains estimates for the gains
-        \alpha_{i|j}
+        \alpha_{i|j}. This array is only calculated if there are no hidden
+        nodes; it's set to zero otherwise.
     cov_mat: np.array of shape=(dim, dim)
-        The covariance matrix calculated from the input datset
+        The covariance matrix calculated from the input dataset
     cum_err: float
-        cummulative error, equal to the sum of the absolute values of the
-        errors err_i_j in gains_sb_list.
+        cumulative error, equal to the sum of the absolute values of the
+        errors err_i_j in gains_sb_list. This error is calculated only if
+        there are no hidden nodes; it's set to zero otherwise.
     gains_sb_list: list[sp.Equality]
-        A list of equations of the form 'alp_i_L_j = float' if the graph has
-        an arrow x_j->x_i, or of the form 'err_i_j = float' if that arrow is
-        missing from the graph. 'err_i_j' is an error equal to the
+        A list of equations of the form '\alpha_{i|j} = float' if the graph
+        has an arrow x_j->x_i, or of the form 'err_{i,j} = float' if that
+        arrow is missing from the graph. 'err_i_j' is an error equal to the
         difference between both sides of an equation that constrains the
-        covariances.
-
-
+        covariances. Exception: if there are hidden nodes, the right hand
+        sides of these equations may contain symbolic expressions pertaining
+        to covariances alluding to hidden nodes.
+    graph: Graph
+    hidden_nds: list[str] or None
+        This is a list of the nodes that are hidden.
     """
 
-    def __init__(self, graph, path, gains_calculator):
+    def __init__(self, graph, path, gains_calculator,
+                 hidden_nds=None):
         """
 
         Parameters
@@ -43,13 +55,20 @@ class GainsEstimator:
         path: str
             path to input file containing dataset
         gains_calculator: GainsCalculator
+        hidden_nds: list[str] or None
         """
+        self.graph = graph
         df = pd.read_csv(path)
         assert list(df.columns) == graph.ord_nodes
         self.cov_mat = df.cov().to_numpy()
         # print("ddfgh", df.cov())
         self.gains_sb_list = deepcopy(gains_calculator.gains_sb_list)
         assert self.gains_sb_list is not None
+        if hidden_nds is None:
+            self.hidden_nds = []
+        else:
+            assert set(hidden_nds).issubset(graph.ord_nodes)
+            self.hidden_nds = hidden_nds
         dim = graph.num_nds
         self.alp_mat_estimate = np.zeros((dim, dim))
         self.cum_err = 0
@@ -60,25 +79,32 @@ class GainsEstimator:
                 str1 = "err" + str0[3:len(str0)]
                 eq = sp.Eq(sp.Symbol(str1), eq.args[0]-eq.args[1])
             for row, col in product(range(dim), range(dim)):
-                if row <= col:
+                row_nd = graph.ord_nodes[row]
+                col_nd = graph.ord_nodes[col]
+                symbolic = (row_nd in self.hidden_nds) or\
+                          (col_nd in self.hidden_nds)
+                if row <= col and not symbolic:
                     cov_sb = sp.Symbol("cov_" +
                          str(row) + "_" + str(col))
                     eq = eq.subs({cov_sb: self.cov_mat[row, col]})
             self.gains_sb_list[i] = eq
-            str1 = str(eq.args[1])
-            if str0[0:3] == "alp":
-                row_str, col_str = str0[4:len(str0)].split("_L_")
-                row, col = int(row_str), int(col_str)
-                self.alp_mat_estimate[row, col] = float(str1)
-            else:
-                self.cum_err += abs(float(str1))
+            if len(self.hidden_nds) == 0:
+                str1 = str(eq.args[1])
+                if str0[0:3] == "alp":
+                    row_str, col_str = str0[4:len(str0)].split("_L_")
+                    row, col = int(row_str), int(col_str)
+                    self.alp_mat_estimate[row, col] = float(str1)
+                else:
+                    self.cum_err += abs(float(str1))
 
-    def print_gains(self, true_alp_mat=None):
+    def print_gains(self, true_alp_mat=None, verbose=False):
         """
-        This method prints the estimates of the gains \alp_{i|j} if arrow
-        $x_j->x_i$ is present in the DAG, or of err_i_j if that arrow is
-        absent in the DAG. It also prints, if it's available, the true
-        \alp_{ i|j} next to its estimate
+        This method renders in latex, in a jupyter notebook (but not on the
+        console), the estimates of the gains \alp_{i|j} if arrow $x_j->x_i$
+        is present in the DAG, or of err_i_j if that arrow is absent in the
+        DAG. It also prints, if it's available, the true \alp_{i|j} next to
+        its estimate. Iff verbose=True, it also prints the same thing in ASCII,
+        in both the console and jupyter notebook.
 
         Parameters
         ----------
@@ -86,24 +112,52 @@ class GainsEstimator:
             This input contains the true alpha matrix alp_mat, if one is
             known. One would be known if the input dataset was generated by
             RandomDataMaker.
+        verbose: bool
 
         Returns
         -------
         None
 
         """
+        full_str = r"\begin{array}{l}" + "\n"
         for eq in self.gains_sb_list:
             str0 = str(eq.args[0])
-            str1 = "%.6f" % float(eq.args[1])
-            str01 = str0 + "= " + str1
-            if str(eq.args[0])[0:3] != "alp" or true_alp_mat is None:
-                print("\n", str01)
-            else:
+            alp_eq = True
+            if str(eq.args[0])[0:3] == "alp":
                 row_str, col_str = str0[4:len(str0)].split("_L_")
-                row, col = int(row_str), int(col_str)
+            else:
+                alp_eq = False
+                row_str, col_str = str0[4:len(str0)].split("_")
+            row, col = int(row_str), int(col_str)
+            row_nd = self.graph.ord_nodes[row]
+            col_nd = self.graph.ord_nodes[col]
+            if alp_eq:
+                sb_str0 = r"\alpha_{\underline{" + row_nd + "}|"
+            else:
+                sb_str0 = r"err_{\underline{" + row_nd + "},"
+            sb_str0 += r"\underline{" + col_nd + r"}}"
+            if len(self.hidden_nds) == 0:
+                str1 = "%.6f" % float(eq.args[1])
+            else:
+                def round_expr(expr, num_digits):
+                    return expr.xreplace(
+                        {n: round(n, num_digits) for n in expr.atoms(
+                            sp.Number)})
+
+                str1 = round_expr(eq.args[1], 6)
+                str1 = do_latex_subs(self.graph, str1)
+                str1 = sp.latex(str1)
+            str01 = sb_str0 + "= " + str1
+            if not alp_eq or true_alp_mat is None:
+                full_str += str01 + r"\\" + "\n"
+            else:
                 true = true_alp_mat[row, col]
                 true_str = "%.6f" % true
-                print("\n", str01, ", true=", true_str)
+                full_str += str01 + r",\quad true=" + true_str + r"\\" + "\n"
+        full_str = full_str[:-3] + "\n" + r"\end{array}"
+        if verbose:
+            print(full_str)
+        return sp.Symbol(full_str)
 
 
 if __name__ == "__main__":
@@ -127,8 +181,10 @@ if __name__ == "__main__":
         calc = GainsCalculator(graph)
         calc.calculate_gains_sb()
         gest = GainsEstimator(graph, data_path, calc)
-        gest.print_gains(true_alp_mat=dmaker.alp_mat)
+        gest.print_gains(true_alp_mat=dmaker.alp_mat, verbose=True)
         print("alp_mat_estimate=\n", gest.alp_mat_estimate)
         print("cum_err=", gest.cum_err)
+        gest = GainsEstimator(graph, data_path, calc, hidden_nds=["s"])
+        gest.print_gains(true_alp_mat=dmaker.alp_mat, verbose=True)
 
     main()
