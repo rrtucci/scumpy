1dac6b463cadb8dbfb4f6870c37f77bff9885a67
rrtucci <tucci@ar-tiste.com>
2023-02-27 02:39:19 -0500

    added first version of linear SCM with feedback loops

--- FBackGEmanager.py
+++ FBackGEmanager.py
@@ -23,11 +23,11 @@ class FBackGEmanager:
         df = df[columns]
         dim = self.graph.num_nds
         self.n_to_estimator = {}
-        for time in range(1, self.n_max-1):
+        for time in range(1, self.n_max):
             slice_n = columns[(time-1)*dim : time*dim]
             slice_n_plus_one = columns[time * dim: (time+1) * dim]
             two_slices = slice_n + slice_n_plus_one
-            df_two_slices = df(two_slices)
+            df_two_slices = df[two_slices]
             self.n_to_estimator[time] = FBackGainsEstimator(
                 time,
                 graph,
@@ -36,3 +36,27 @@ class FBackGEmanager:
                 hidden_nds
             )
             self.n_to_estimator[time].calculate_gains()
+
+    def print_alpha_lists(self, true_alpha_mat=None, verbose=False):
+        str0 = r"\begin{array}{l}" + "\n"
+        for time in range(1, self.n_max):
+            str0 += r"\text{******** time=" + str(time) + r"}\\" + "\n"
+            gest = self.n_to_estimator[time]
+            x = gest.print_alpha_list(true_alpha_mat=true_alpha_mat,
+                                  verbose=verbose)
+            str0 += str(x) + "\n" + r"\\" + "\n"
+        str0 = str0[:-3]
+        str0 += r"\end{array}"
+        return sp.Symbol(str0)
+    
+    def print_beta_lists(self, true_beta_mat=None, verbose=False):
+        str0 = r"\begin{array}{l}" + "\n"
+        for time in range(1, self.n_max):
+            str0 += r"\text{******** time=" + str(time) + r"}\\" + "\n"
+            gest = self.n_to_estimator[time]
+            x = gest.print_beta_list(true_beta_mat=true_beta_mat,
+                                  verbose=verbose)
+            str0 += str(x) + "\n" + r"\\" + "\n"
+        str0 = str0[:-3]
+        str0 += r"\end{array}"
+        return sp.Symbol(str0)
